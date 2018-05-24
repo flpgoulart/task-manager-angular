46c816d4468f6f5a30e76805a953e0ddb8e4e5c4
Felipe Goulart <flpgoulart@gmail.com>
2018-05-24 12:34:12 -0300

    adding navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+// todos os component tem essa chamada para dizer que é componente
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: './navbar.component.html'
+})
+
+export class NavbarComponent {
+
+}
