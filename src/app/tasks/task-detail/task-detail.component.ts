f33ca90248692223ed78d63aac3d2789792cce86
Felipe Goulart <flpgoulart@gmail.com>
2018-07-17 11:55:50 -0300

    getting params from TaskDetailComponent route

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,17 +1,36 @@
 // como prática, indica importar sempre o Component em primeiro lugar
 //o Input permite a troca de variáveis dentro do HTML, ou seja, passagem de param de um modelo para outro
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+// ActivatedRoute dá detalhes ao que se está acessando pelo usuário
+// Params é para coletar os dados transferidos pela URL
+import { ActivatedRoute, Params } from "@angular/router";
+// serve para implementar no route o método switchMap
+import "rxjs/add/operator/switchMap";
 
 import { Task } from '../shared/task.model';
+import { TaskService } from "../shared/task.service";
 
 @Component({
     selector: 'task-detail',
     templateUrl: './task-detail.component.html'
 })
 
-export class TaskDetailComponent{
+export class TaskDetailComponent implements OnInit{
     @Input() public task: Task;
 
-    public constructor() {
+    public constructor(
+        private taskService: TaskService,
+        private route: ActivatedRoute
+    ) {
+         
+    }
+
+    public ngOnInit(){
+        this.route.params
+        // no caso do subscribe é por causa do Observable, identico ao Promise
+        // o switchMap ele é inteligente o suficiente para tratar várias chamadas de requisições e entregar somente a ultima para o subscribe retornar
+            .switchMap((params: Params) => this.taskService.getTask(+params['id']))
+            //o subscribe sempre precisa estar presente
+            .subscribe(task => this.task = task)
     }
 }
